06d61d672104beecf05d00da6c78004461fccac0
Sceinox <sc31n0x@googlemail.com>
2016-11-13 02:18:35 +0100

    GunListActivity-Buttons work now

--- app/src/main/java/sceinox/atragmx/GunlistActivity.java
+++ app/src/main/java/sceinox/atragmx/GunlistActivity.java
@@ -1,40 +1,40 @@
 package sceinox.atragmx;
 
-
-
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class GunlistActivity extends AppCompatActivity {
+public class GunlistActivity extends AppCompatActivity{
 
-
-    public GunlistActivity(MainActivity mainActivity){
-        mainActivity.setContentView(R.layout.activity_gunlist);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gunlist);
     }
 
     //region onClickEvents
-    public void onOpenClick(View view){
-        System.out.println("1");
+    public void onOpenClick(View view) {
+        System.out.println("test");
     }
 
-    public void onSaveClick(View view){
-        System.out.println("2");
+    public void onSaveClick(View view) {
+        System.out.println("test");
     }
 
-    public void onAddClick(View view){
-        System.out.println("3");
+    public void onAddClick(View view) {
+        System.out.println("test");
     }
 
-    public void onDeleteClick(View view){
-        System.out.println("4");
+    public void onDeleteClick(View view) {
+        System.out.println("test");
     }
 
-    public void onNoteClick(View view){
-        System.out.println("5");
+    public void onNoteClick(View view) {
+        System.out.println("test");
     }
 
-    public void onDoneClick(View view){
-        System.out.println("6");
+    public void onDoneClick(View view) {
+        System.out.println("test");
     }
     //endregion
 
